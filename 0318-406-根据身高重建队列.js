a2c0e0c4e6e70540e7908617ba3db2af814d1933
Dannyzn <694518723@qq.com>
2022-03-20 21:16:08 +0800

    0318-406-根据身高重建队列-代码实现

--- 0318-406-根据身高重建队列.js
+++ 0318-406-根据身高重建队列.js
@@ -39,3 +39,23 @@
 
 
 
+// 代码实现:
+
+var reconstructQueue = function(people) {
+    let queue = [];
+
+    // 第一大步
+    people.sort((a, b) => {
+        // 1.1
+        if (a[0] !== b[0]) {
+            return b[0] - a[0]
+        } else {
+            return a[1] - b[1]
+        }
+    })
+
+    // 第二大步
+    for (let i = 0; i < people.length; i++) {
+        queue.splice(people[i][1], 0, people[i])
+    }
+}
