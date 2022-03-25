f63783932796662efc2b6d4cb800cbba489752da
Dannyzn <694518723@qq.com>
2022-03-25 21:11:28 +0800

    0325-用最少数量的箭引爆气球

--- 202203/0321-452. 用最少数量的箭引爆气球.js
+++ 202203/0321-452. 用最少数量的箭引爆气球.js
@@ -18,3 +18,22 @@
 // 输入：points = [[1,2],[2,3],[3,4],[4,5]]
 // 输出：2
 
+
+// 代码实现: 
+var findMinArrowShots = function(points) {
+    points.sort((a, b) => {
+        return a[0] - b[0];
+    })
+
+    let result = 1;  // point 不为空至少需要一支箭
+    for (let i = 1; i < points.length; i++) {
+        if (points[i][0] > points[i - 1][1]) {
+            result++
+        } else {
+            points[i][1] = Math.min(points[i-1][1], points[i][1])
+        }
+    }
+
+    return result;
+}
+
